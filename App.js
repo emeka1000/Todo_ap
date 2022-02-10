8e30dd3cd08f8da573a9ffd8a5963e41a61f898b
Emeka Okonkwo <hycienth1000@gmail.com>
2022-02-10 00:15:47 -0800

    Modal added

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
 import GoalInput from "./components/GoalInput";
 
 import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [Goals, setGoals] = useState([]);
+  const [openAddMode, setOpenAddMode] = useState(false);
 
   const addGoalHandler = (goalTitle) => {
     setGoals((currentGoals) => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle },
     ]);
+    setOpenAddMode(false);
   };
 
   const removeGoalHandler = (goalId) => {
@@ -31,7 +25,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title={"Add New Goal"} onPress={() => setOpenAddMode(true)} />
+      <GoalInput visible={openAddMode} onAddGoal={addGoalHandler} />
       <Text>Tap to delete</Text>
       <FlatList
         keyExtractor={(item, index) => item.id}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 
 const GoalInput = (props) => {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -9,31 +9,35 @@ const GoalInput = (props) => {
   };
 
   return (
-    <View style={styles.content}>
-      <TextInput
-        placeholder="Enter todo"
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-      />
-      <Button title="Add" onPress={props.onAddGoal.bind(this, enteredGoal)} />
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.content}>
+        <TextInput
+          placeholder="Enter todo"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   content: {
-    width: "80%",
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   input: {
     borderColor: "black",
     borderWidth: 1,
     padding: 8,
-    width: "70%",
+    width: "80%",
     borderRadius: 50,
+    marginBottom: 10,
   },
 });
 
